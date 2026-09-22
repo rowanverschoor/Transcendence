4e1a543979588707dfb094e72fc255aea7a085ea
a0f <showard@student.codam.nl>
2026-09-22 13:53:38 +0200

    swapped out inline template and style

--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -4,20 +4,7 @@ import { RouterOutlet, RouterLink } from '@angular/router';
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet, RouterLink],
-  template: `<nav>
-  <a routerLink="/">Home</a>
-  <a routerLink="/profile">Profile</a>
-  <a routerLink="/game">Game</a>
-  <a routerLink="/login">Login</a>
-  <a routerLink="/register">Register</a>
-  <a routerLink="/privacy">Privacy</a>
-  <a routerLink="/terms">Terms</a>
-</nav>
-<router-outlet />`,
-  styles: `
-    :host {
-      color: #a144eb;
-    }
-  `,
+  templateUrl: './app.html',
+  styleUrl: './app.scss',
 })
 export class App {}
