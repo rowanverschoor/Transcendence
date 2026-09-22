107962e45c73b0f7180b67a7b5bafdffae6d6a62
Nils Hendriks <nhendrik@student.codam.nl>
2026-09-22 13:06:26 +0200

    added sprite to player

--- player.ts
+++ player.ts
@@ -1,12 +1,16 @@
+import { gameObject } from "./gameObject";
+
 class Player {
 	size: number;
 	x: number;
 	y: number;
+	sprite: gameObject;
 
-	constructor(size: number, x: number, y: number) {
+	constructor(size: number, x: number, y: number, sprite: gameObject) {
 		this.size = size;
 		this.x = x;
 		this.y = y;
+		this.sprite = sprite;
 	}
 
 	translate2D(x: number, y: number) {
